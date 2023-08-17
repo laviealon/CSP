8e3bfe56969fadbd89c8c54dd23dd91ef554ec95
laviealon <79278018+laviealon@users.noreply.github.com>
2023-08-17 16:45:20 -0400

    Completed CSP with forward-checking!

--- assignment.py
+++ assignment.py
@@ -26,6 +26,32 @@ class CSP:
     def backtrack(self, assignment):
         return backtrack(self, assignment)
 
+    def inference(self, assignment):
+        new_domains = dict()
+        for var in self.variables:
+            inference_domain = []
+            if var in assignment:
+                continue
+            for value in self.domains[var]:
+                assignment[var] = value
+                if self.consistent(assignment):
+                    inference_domain.append(value)
+                assignment.pop(var)
+            if len(inference_domain) == 0:
+                return None
+            new_domains[var] = inference_domain
+        return new_domains
+
+    def update(self, inferences):
+        old_domains = self.domains.copy()
+        for var in inferences:
+            self.domains[var] = inferences[var]
+        return old_domains
+
+    def restore(self, old_domains):
+        self.domains = old_domains
+
+
 def backtracking_search(csp):
     return csp.backtrack({})
 
@@ -33,17 +59,22 @@ def order_domain_values(var, assignment, csp):
     return csp.domains[var]
 
 
-def backtrack(csp, assignment):
+def backtrack(csp: CSP, assignment):
     if csp.check_assignment_complete(assignment):
         return assignment
     var = csp.select_unassigned_variable(assignment)
     for value in order_domain_values(var, assignment, csp):
         assignment[var] = value
+        inferences = csp.inference(assignment)
+        if inferences is not None:
+            old_domains = csp.update(inferences)
         if csp.consistent(assignment):
             result = backtrack(csp, assignment)
             if result is not None:
                 return result
         assignment.pop(var)
+        if inferences is not None:
+            csp.restore(old_domains)
     return None
 
 
